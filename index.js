9e0fdb039c879cef74ad506272eea9c3c0e1318f
storm <yves.straten@gmail.com>
2023-09-10 12:14:00 +0000

    Fully transitioned to mongoDB, sorting, limiting, from & to work

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const parser = require("body-parser");
+const { MongoClient } = require("mongodb")
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.static("public"));
+
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("exercise-tracker")
+const users = db.collection("users")
+const exercises = db.collection("exercises")
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/views/index.html");
@@ -17,143 +23,78 @@ app.use(
 	}),
 );
 
-let users = [];
-
-let exercises = [];
-
-const checkId = (sentId) => {
-	const findId = users.find((users) => {
-		return users._id === sentId;
-	});
-
-	console.log(findId);
-
-	return findId;
-};
-
-app.post("/api/users/:_id/exercises", (req, res) => {
+app.post("/api/users/:_id/exercises", async (req, res) => {
 	const description = req.body.description;
 	const duration = parseInt(req.body.duration);
 	const date = new Date(req.body.date).toDateString();
 
-	if (checkId(req.params._id)) {
+	if (users.findOne({ id: req.params._id })) {
+		await exercises.insertOne({
+			id: req.params._id,
+			description: description,
+			duration: duration,
+			date: date,
+			hiddenDate: new Date(req.body.date)
+		});
+
 		res.json({
 			_id: req.params._id,
 			description: description,
 			duration: duration,
 			date: date,
 		});
-
-		exercises.push({
-			_id: req.params._id,
-			description: description,
-			duration: duration,
-			date: date,
-		});
-		console.log(exercises);
 	}
 });
 
-app.get("/api/users/:_id/logs", (req, res) => {
-	const getUsrName = (input) => {
-		const findUsr = users.find((users) => users._id === input);
+app.get("/api/users/:_id/logs", async (req, res) => {
+	const limit = parseInt(req.query.limit)
+	const from = req.query.from ? new Date(req.query.from) : null
+	const to = req.query.to ? new Date(req.query.to) : null
+	console.log(new Date(from))
+	console.log(new Date(to))
 
-		if (findUsr) {
-			return findUsr.user;
-		} else {
-			console.log("No user with this ID");
-		}
-	};
-
-	const getExercises = (_id, limit, from, to) => {
-		const matchIdWithExercise = exercises
-			.filter((exercises) => exercises._id === _id)
-			.sort((a, b) => new Date(a.date) - new Date(b.date));
-
-		const fromTo = matchIdWithExercise.filter((exercises) => {
-			const fromDate = new Date(from);
-			const toDate = new Date(to);
-			return (
-				new Date(exercises.date) >= fromDate &&
-				new Date(exercises.date) <= toDate
-			);
-		});
-
-		if (from && to) {
-			if (limit) {
-				const fromToSliced = fromTo.slice(0, limit);
-				return fromToSliced;
-			} else {
-				return fromTo;
-			}
-		} else if (limit) {
-			const limitSlice = matchIdWithExercise.slice(0, limit);
-			console.log(limitSlice);
-			return limitSlice;
-		} else {
-			return matchIdWithExercise;
-		}
-	};
-
-	const countExercises = (_id, limit) => {
-		let count = 0;
-		if (limit) {
-			count = limit;
-		} else {
-			exercises.forEach((exercise) => {
-				if (exercise._id === _id) {
-					count++;
-				}
-			});
+	if (await users.findOne({ id: req.params._id })) {
+		const query = {
+			hiddenDate: {
+				$lte: to,
+				$gte: from
+			},
+			id: req.params._id
 		}
 
-		return count;
-	};
-
-	const findUsrName = getUsrName(req.params._id);
-
-	if (checkId(req.params._id)) {
-		const from = req.query.from;
-		const to = req.query.to;
-		const limit = parseInt(req.query.limit);
-
-		if (limit || (from && to)) {
-			const findExercisesLimit = getExercises(req.params._id, limit, from, to);
-			const countingToLimit = countExercises(req.params._id, limit);
-
-			res.json({
-				username: findUsrName,
-				count: countingToLimit,
-				_id: req.params._id,
-				log: findExercisesLimit,
-			});
-		} else {
-			const findExercises = getExercises(req.params._id);
-			console.log(findExercises);
-			const counting = countExercises(req.params._id);
-
-			res.json({
-				username: findUsrName,
-				count: counting,
-				_id: req.params._id,
-				log: findExercises,
-			});
+		const sort = {
+			hiddenDate: 1
 		}
+
+		const projection = {
+			_id: 0, id: 0, hiddenDate: 0
+
+		}
+		const log = exercises.find(query).project(projection).limit(limit).sort(sort)
+		const allExercises = await log.toArray()
+
+		const count = limit ? limit : await exercises.countDocuments({ id: req.params._id })
+
+		res.json({
+			_id: req.params._id,
+			count: count,
+			log: allExercises,
+		})
+	} else {
+		res.json({
+			error: "user not found"
+		})
 	}
 });
 
 app.post("/api/users", (req, res) => {
-	const createUser = (username) => {
-		const id = crypto.randomUUID();
-		users.push({ user: username, _id: id });
+	const username = req.body.username;
 
-		return id;
-	};
-
-	const id = createUser(req.body.username);
+	const id = crypto.randomUUID();
+	users.insertOne({ user: username, id: id });
 
 	res.json({
-		username: req.body.username,
+		username: username,
 		_id: id,
 	});
 });
